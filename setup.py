8af7bbe10c9f5661a7b10f2f1d3ef37362f3e773
Pedro H <5179251+pedrohdz@users.noreply.github.com>
2018-12-15 19:55:46 +0100

    Eliminating requirements*.txt and moving more to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import re
+import sys
 import ast
 from setuptools import setup, find_packages
 
@@ -17,6 +18,23 @@ def long_description():
     with open('README.rst', 'rb') as handle:
         return handle.read().decode('utf-8')
 
+
+_TEST_REQUIRE = [
+    'pyfakefs>=3.5',
+    'pytest-cov>=2.5.1',
+    'pytest>=3.0.7',
+]
+
+if sys.version_info[0] < 3:
+    _TEST_REQUIRE.extend(['mock'])
+
+_CI_REQUIRE = [
+    'coverage>=4.4.1',
+    'flake8>=3.3.0',
+    'pep257>=0.7.0',
+    'pylint>=1.7.1',
+    'tox>=2.7.0',
+]
 
 setup(
     name='py-lambda-packer',
@@ -41,11 +59,19 @@ setup(
     keywords='aws lambda',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
+    setup_requires=[
+        'pytest-runner',
+    ],
     install_requires=[
         'PyYAML>=3.12',
         'colorlog>=2.10.0',
-        'future>=0.16.0'
+        'future>=0.16.0',
     ],
+    tests_require=_TEST_REQUIRE,
+    extras_require={
+        'ci': _CI_REQUIRE,
+        'test': _TEST_REQUIRE,
+    },
     entry_points={'console_scripts': [
         'py-lambda-packer = {}'.format(_ENTRY_POINT),
         'plp = {}'.format(_ENTRY_POINT),
