1f49b272b53561b694bb87bae0b50d58ff63011e
Pedro <5179251+pedrohdz@users.noreply.github.com>
2018-12-15 15:09:07 +0100

    Updated methods called in pyfakefs

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,39 +20,44 @@ def source_fs(request):
     patcher.setUp()
     request.addfinalizer(patcher.tearDown)
 
-    patcher.fs.CreateFile('/home/foo/src/bar-project/config.json')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/.gitignore')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/.git/config')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/py-lambda-packer.yaml')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/templates/images/index.png')  # noqa
-    patcher.fs.CreateFile('/home/foo/src/bar-project/templates/index.html')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/static/images/hello.png')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/static/images/thumb.png')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/static/images/large.png')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/static/images/large.jpg')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/static/images/large.gif')
-    patcher.fs.CreateDirectory('/home/foo/src/bar-project/static/css')
-    patcher.fs.CreateDirectory('/home/foo/src/bar-project/static/js')
-    patcher.fs.CreateFile('/home/foo/src/config/global-config.json')
+    patcher.fs.create_file('/home/foo/src/bar-project/config.json')
+    patcher.fs.create_file('/home/foo/src/bar-project/.gitignore')
+    patcher.fs.create_file('/home/foo/src/bar-project/.git/config')
+    patcher.fs.create_file('/home/foo/src/bar-project/py-lambda-packer.yaml')
+    patcher.fs.create_file(
+        '/home/foo/src/bar-project/templates/images/index.png')
+    patcher.fs.create_file('/home/foo/src/bar-project/templates/index.html')
+    patcher.fs.create_file('/home/foo/src/bar-project/static/images/hello.png')
+    patcher.fs.create_file('/home/foo/src/bar-project/static/images/thumb.png')
+    patcher.fs.create_file('/home/foo/src/bar-project/static/images/large.png')
+    patcher.fs.create_file('/home/foo/src/bar-project/static/images/large.jpg')
+    patcher.fs.create_file('/home/foo/src/bar-project/static/images/large.gif')
+    patcher.fs.create_dir('/home/foo/src/bar-project/static/css')
+    patcher.fs.create_dir('/home/foo/src/bar-project/static/js')
+    patcher.fs.create_file('/home/foo/src/config/global-config.json')
 
-    patcher.fs.CreateFile('/home/foo/src/bar-project/posts/a/b/c/d/bw.html')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/posts/a1/b/diff/bw.html')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/posts/a/b/c/d/e/bar.html')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/posts/a/b/c/d/e/got.html')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/posts/a/b/c/d/tess.html')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/posts/a/b/c/d/tess.txt')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/posts/a/ref-90.html')
-    patcher.fs.CreateFile('/home/foo/src/bar-project/posts/bucket/link-00.html')  # noqa
-    patcher.fs.CreateFile('/home/foo/src/bar-project/posts/bucket/link-03.html')  # noqa
+    patcher.fs.create_file('/home/foo/src/bar-project/posts/a/b/c/d/bw.html')
+    patcher.fs.create_file('/home/foo/src/bar-project/posts/a1/b/diff/bw.html')
+    patcher.fs.create_file(
+        '/home/foo/src/bar-project/posts/a/b/c/d/e/bar.html')
+    patcher.fs.create_file(
+        '/home/foo/src/bar-project/posts/a/b/c/d/e/got.html')
+    patcher.fs.create_file('/home/foo/src/bar-project/posts/a/b/c/d/tess.html')
+    patcher.fs.create_file('/home/foo/src/bar-project/posts/a/b/c/d/tess.txt')
+    patcher.fs.create_file('/home/foo/src/bar-project/posts/a/ref-90.html')
+    patcher.fs.create_file(
+        '/home/foo/src/bar-project/posts/bucket/link-00.html')
+    patcher.fs.create_file(
+        '/home/foo/src/bar-project/posts/bucket/link-03.html')
 
-    patcher.fs.CreateLink(
+    patcher.fs.create_symlink(
         '/home/foo/src/bar-project/posts/a/b/c/d/symlink-dir',
         '/home/foo/src/bar-project/posts/bucket')
-    patcher.fs.CreateLink(
+    patcher.fs.create_symlink(
         '/home/foo/src/bar-project/posts/links/tef-90.html',
         '/home/foo/src/bar-project/posts/a/b/dpo.html')
 
-    patcher.fs.CreateDirectory('/home/foo/tmp')
+    patcher.fs.create_dir('/home/foo/tmp')
 
     return patcher.fs
 
